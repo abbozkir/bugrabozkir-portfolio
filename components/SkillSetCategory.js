95cbc4a29b386b436ad58e7e71161914af486c78
abbozkir <ali.bugra.bozkir@gmail.com>
2023-11-04 15:24:10 +0100

    Added skill levels

--- components/SkillSetCategory.js
+++ components/SkillSetCategory.js
@@ -5,13 +5,17 @@ const SkillSetCategory = ({title, skills}) => {
     return (
         <div className="card mb-3" >
             <h5>{title}</h5>
-            {skills.map((skill, index) => (
-                <Chip
-                    key={index}
-                    className='my-1 mr-1 bg-primary text-primary text-lg'
-                    label={skill}
-                />
-            ))}
+            {skills.map((skill, index) => {
+                const skillLevelHighlight = skill.level === 'medium' ? 'opacity-60'
+                    : skill.level === 'low' ? 'opacity-30' : '';
+                return (
+                    <Chip
+                        key={index}
+                        className={`my-1 mr-1 text-lg bg-primary ${skillLevelHighlight}`}
+                        label={skill.title}
+                    />
+                );
+            })}
         </div>
     );
 };
